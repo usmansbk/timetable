88d2d6b0d63dea303d5fb5256ef14664903ac86c
usmansbk <usmansbk@gmail.com>
2022-10-20 23:35:49 +0100

    add fonts

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,5 +1,9 @@
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
-import {Text, useTheme} from 'react-native-paper';
+import {useTheme} from 'react-native-paper';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './Home';
+
+const Stack = createNativeStackNavigator();
 
 export default function Screens() {
   const {colors, dark} = useTheme();
@@ -11,7 +15,9 @@ export default function Screens() {
         backgroundColor={colors.primary}
         barStyle={dark ? 'dark-content' : 'light-content'}
       />
-      <Text>Timetable</Text>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen component={Home} name="Home" />
+      </Stack.Navigator>
     </SafeAreaView>
   );
 }
