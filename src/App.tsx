cd165c27047794d725ac71ab7e6ddbc731fd1b66
usmansbk <usmansbk@gmail.com>
2022-10-23 10:38:29 +0100

    configure store

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider as PaperProvider} from 'react-native-paper';
+import {Provider as StoreProvider} from 'react-redux';
 import {useAppTheme} from '~config/theme';
+import store from '~redux/store';
 import Screens from '~screens';
 
 function Main() {
@@ -17,5 +19,9 @@ function Main() {
 }
 
 export default function App() {
-  return <Main />;
+  return (
+    <StoreProvider store={store}>
+      <Main />
+    </StoreProvider>
+  );
 }
