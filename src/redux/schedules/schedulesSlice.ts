14741063270ee27d0d2888823172dd2922061af9
usmansbk <usmansbk@gmail.com>
2022-10-23 12:09:01 +0100

    setup schedules slice

--- src/redux/schedules/schedulesSlice.ts
+++ src/redux/schedules/schedulesSlice.ts
@@ -1,12 +1,22 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {ScheduleInput} from '~types';
+import type {RootState} from '../store';
+
+const initialState: ScheduleInput[] = [];
 
 const schedulesSlice = createSlice({
   name: 'schedules',
-  initialState: [],
+  initialState,
   reducers: {
-    addSchedule(state, action) {},
+    addSchedule(state, action: PayloadAction<ScheduleInput>) {},
+    updateSchedule(state, action: PayloadAction<ScheduleInput>) {},
+    deleteSchedule(state, action: PayloadAction<string>) {},
   },
 });
 
-export const {addSchedule} = schedulesSlice.actions;
+export const {addSchedule, updateSchedule, deleteSchedule} =
+  schedulesSlice.actions;
+
+export const selectSchedules = (state: RootState) => state.schedules;
+
 export default schedulesSlice.reducer;
