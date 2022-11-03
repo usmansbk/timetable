0d1d6e7f36dac73e2a4ded3da0bd2e0ae3eea923
usmansbk <usmansbk@gmail.com>
2022-11-03 23:21:18 +0100

    setup notification

--- src/config/notifications.ts
+++ src/config/notifications.ts
@@ -2,11 +2,22 @@ import PushNotificationIOS from '@react-native-community/push-notification-ios';
 import {Platform} from 'react-native';
 import PushNotification from 'react-native-push-notification';
 
+export const CHANNEL_ID = 'timetable';
+
 PushNotification.configure({
   onNotification: notification => {
     notification.finish(PushNotificationIOS.FetchResult.NoData);
   },
   requestPermissions: Platform.OS === 'ios',
 });
+
+PushNotification.createChannel(
+  {
+    channelId: CHANNEL_ID,
+    channelName: 'Timetable',
+    channelDescription: 'Timetable reminder',
+  },
+  () => null,
+);
 
 export default PushNotification;
--- src/screens/Timetable/Agenda.tsx
+++ src/screens/Timetable/Agenda.tsx
@@ -1,6 +1,7 @@
 import {useNavigation} from '@react-navigation/native';
-import {memo, useCallback} from 'react';
+import {memo, useCallback, useEffect} from 'react';
 import AgendaList from '~components/AgendaList';
+import PushNotification, {CHANNEL_ID} from '~config/notifications';
 import {useAppSelector} from '~redux/hooks';
 import {selectAllEvents} from '~redux/timetable/slice';
 import {EventInput} from '~types';
@@ -19,6 +20,17 @@ function Agenda() {
     },
     [navigation],
   );
+
+  useEffect(() => {
+    PushNotification.localNotificationSchedule({
+      channelId: CHANNEL_ID,
+      title: 'test',
+      message: 'hello',
+      allowWhileIdle: true,
+      date: new Date(Date.now() + 6 * 1000),
+      repeatTime: 1,
+    });
+  }, []);
 
   return <AgendaList items={events} onPressItem={onPressItem} />;
 }
