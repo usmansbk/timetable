58d7e8213512f3d09bcbe281141ab2801dd7fb41
usmansbk <usmansbk@gmail.com>
2022-11-11 08:55:13 +0100

    nit

--- src/components/PickerInput.tsx
+++ src/components/PickerInput.tsx
@@ -1,13 +1,6 @@
 import {StyleSheet, View} from 'react-native';
-import {
-  Text,
-  TouchableRipple,
-  IconButton,
-  Divider,
-  useTheme,
-} from 'react-native-paper';
+import {Text, TouchableRipple, IconButton, useTheme} from 'react-native-paper';
 
-const MD3_MIN_HEIGHT = 56;
 const MAXIMIZED_LABEL_FONT_SIZE = 16;
 const MINIMIZED_LABEL_FONT_SIZE = 12;
 
@@ -34,50 +27,47 @@ function PickerInput({
   optional,
   error,
   onClear,
-  placeholder,
 }: Props) {
   const {colors} = useTheme();
   return (
-    <>
-      <TouchableRipple onPress={onPress} disabled={disabled}>
-        <View
-          style={[
-            styles.container,
-            {
-              backgroundColor: error
-                ? colors.errorContainer
-                : colors.background,
-            },
-          ]}>
-          {icon ? <IconButton icon={icon} disabled /> : undefined}
-          <View style={styles.body}>
-            {label && (
-              <Text
-                variant="labelLarge"
-                style={{
-                  fontSize: value
-                    ? MINIMIZED_LABEL_FONT_SIZE
-                    : MAXIMIZED_LABEL_FONT_SIZE,
-                  color: colors.outline,
-                }}>
-                {label}
-              </Text>
-            )}
-            {value && <Text variant="bodyLarge">{value}</Text>}
-          </View>
-          {optional && value ? (
-            <IconButton icon="close" onPress={onClear} />
-          ) : null}
+    <TouchableRipple onPress={onPress} disabled={disabled}>
+      <View
+        style={[
+          styles.container,
+          {
+            backgroundColor: error ? colors.errorContainer : colors.background,
+          },
+        ]}>
+        {icon ? <IconButton icon={icon} disabled /> : undefined}
+        <View style={styles.body}>
+          {label && (
+            <Text
+              variant="labelLarge"
+              style={{
+                fontSize: value
+                  ? MINIMIZED_LABEL_FONT_SIZE
+                  : MAXIMIZED_LABEL_FONT_SIZE,
+                color: colors.outline,
+              }}>
+              {label}
+            </Text>
+          )}
+          {value && (
+            <Text style={{color: colors.onSurfaceVariant}} variant="bodyLarge">
+              {value}
+            </Text>
+          )}
         </View>
-      </TouchableRipple>
-      <Divider />
-    </>
+        {optional && value ? (
+          <IconButton icon="close" onPress={onClear} />
+        ) : null}
+      </View>
+    </TouchableRipple>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    minHeight: MD3_MIN_HEIGHT,
     flexDirection: 'row',
     alignItems: 'center',
     paddingVertical: 8,
