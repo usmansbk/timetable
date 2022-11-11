6f63fadd47b7d540077ecdc36448b540779979f3
usmansbk <usmansbk@gmail.com>
2022-11-11 08:39:26 +0100

    fix picker input

--- src/components/PickerInput.tsx
+++ src/components/PickerInput.tsx
@@ -1,5 +1,15 @@
-import {TouchableOpacity} from 'react-native';
-import {TextInput} from 'react-native-paper';
+import {StyleSheet, View} from 'react-native';
+import {
+  Text,
+  TouchableRipple,
+  IconButton,
+  Divider,
+  useTheme,
+} from 'react-native-paper';
+
+const MD3_MIN_HEIGHT = 56;
+const MAXIMIZED_LABEL_FONT_SIZE = 16;
+const MINIMIZED_LABEL_FONT_SIZE = 12;
 
 interface Props {
   value: string;
@@ -25,29 +35,56 @@ function PickerInput({
   error,
   onClear,
   placeholder,
-  multiline,
 }: Props) {
+  const {colors} = useTheme();
   return (
-    <TouchableOpacity disabled={disabled} onPress={onPress}>
-      <TextInput
-        theme={{
-          roundness: 0,
-        }}
-        multiline={multiline}
-        value={value}
-        label={label}
-        placeholder={placeholder}
-        editable={false}
-        left={icon ? <TextInput.Icon icon={icon} disabled /> : undefined}
-        right={
-          optional && value ? (
-            <TextInput.Icon icon="close" onPress={onClear} />
-          ) : null
-        }
-        error={error}
-      />
-    </TouchableOpacity>
+    <>
+      <TouchableRipple onPress={onPress} disabled={disabled}>
+        <View
+          style={[
+            styles.container,
+            {
+              backgroundColor: error
+                ? colors.errorContainer
+                : colors.background,
+            },
+          ]}>
+          {icon ? <IconButton icon={icon} disabled /> : undefined}
+          <View style={styles.body}>
+            {label && (
+              <Text
+                variant="labelLarge"
+                style={{
+                  fontSize: value
+                    ? MINIMIZED_LABEL_FONT_SIZE
+                    : MAXIMIZED_LABEL_FONT_SIZE,
+                  color: colors.outline,
+                }}>
+                {label}
+              </Text>
+            )}
+            {value && <Text variant="bodyLarge">{value}</Text>}
+          </View>
+          {optional && value ? (
+            <IconButton icon="close" onPress={onClear} />
+          ) : null}
+        </View>
+      </TouchableRipple>
+      <Divider />
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    minHeight: MD3_MIN_HEIGHT,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 8,
+  },
+  body: {
+    flex: 1,
+  },
+});
 
 export default PickerInput;
